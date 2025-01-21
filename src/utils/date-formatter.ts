d02a8a274bfd12957c3ce4245ab06296ae34c4f8
Mike S <mksoofian@gmail.com>
2025-01-20 20:22:25 -0500

    fixed dateFormatter  by using .replace(/-/g, '\/')

--- src/utils/date-formatter.ts
+++ src/utils/date-formatter.ts
@@ -3,7 +3,7 @@ export const dateFormatter = (date: string) => {
   // format of date param "1957-04-10", YYYY-MM-DD
   // to format "Apr 10, 1957"
   console.log(date);
-  return new Date(date).toLocaleDateString("en-US", {
+  return new Date(date.replace(/-/g, "/")).toLocaleDateString("en-US", {
     month: "short",
     day: "numeric",
     year: "numeric",
