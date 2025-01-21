fe11fc6a2d6b471df69fde0aeba54c5c3a848516
Mike S <mksoofian@gmail.com>
2025-01-20 20:34:49 -0500

    removed unused console

--- src/utils/date-formatter.ts
+++ src/utils/date-formatter.ts
@@ -1,8 +1,6 @@
-//   .tolocaldatestring subtracts a day... need to fix -- use datefns?
 export const dateFormatter = (date: string) => {
   // format of date param "1957-04-10", YYYY-MM-DD
   // to format "Apr 10, 1957"
-  console.log(date);
   return new Date(date.replace(/-/g, "/")).toLocaleDateString("en-US", {
     month: "short",
     day: "numeric",
