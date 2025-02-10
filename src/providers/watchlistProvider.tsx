f40404dec52978f9dd1d440e8e374eed6f4043a1
Mike S <mksoofian@gmail.com>
2025-02-09 22:46:21 -0500

    updated boiler plate provider but still getting type issues. What is wrong with my setWatchlist in the provider?

--- src/providers/watchlistProvider.tsx
+++ src/providers/watchlistProvider.tsx
@@ -1,37 +1,22 @@
 import {
   createContext,
-  Dispatch,
-  SetStateAction,
+  //   Dispatch,
+  //   SetStateAction,
   useContext,
+  useEffect,
   //   useEffect,
   useState,
 } from "react";
 
 export type GlobalStateType = {
-  firstname: string;
-  lastname: string;
-  age: string;
+  watchlist: string[] | null;
+  setWatchlist: (c: string[] | null) => void;
 };
 
-const GlobalStateContext = createContext({
-  state: {} as Partial<GlobalStateType>,
-  setState: {} as Dispatch<SetStateAction<Partial<GlobalStateType>>>,
+const GlobalStateContext = createContext<GlobalStateType>({
+  watchlist: null,
+  setWatchlist: (c: string[] | null) => {},
 });
-
-const GlobalStateProvider = ({
-  children,
-  value = {} as GlobalStateType,
-}: {
-  children: React.ReactNode;
-  value?: Partial<GlobalStateType>;
-}) => {
-  const [state, setState] = useState(value);
-  return (
-    <GlobalStateContext.Provider value={{ state, setState }}>
-      {children}
-    </GlobalStateContext.Provider>
-  );
-};
 
 const useGlobalState = () => {
   const context = useContext(GlobalStateContext);
@@ -40,64 +25,26 @@ const useGlobalState = () => {
   }
   return context;
 };
+const GlobalStateProvider = ({ children }: { children: React.ReactNode }) => {
+  const [watchlist, setWatchlist] = useState(null);
+
+  useEffect(() => {
+    const localData = localStorage.getItem("watchlist");
+    if (localData) {
+      setWatchlist(JSON.parse(localData));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (watchlist) {
+      localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    }
+  }, [watchlist]);
+  return (
+    <GlobalStateContext.Provider value={{ watchlist, setWatchlist }}>
+      {children}
+    </GlobalStateContext.Provider>
+  );
+};
 
 export { GlobalStateProvider, useGlobalState };
-
-// export const useAuthContext = () => useContext(AuthContext);
-// export function AuthProvider = props => {
-//   const [pageNum, setPageNum] = useState(1);
-//   const [watchlist, setWatchlist] = useState<string[] | null>(null);
-
-//   useEffect(() => {
-//     const localData = localStorage.getItem("watchlist");
-//     if (localData) {
-//       setWatchlist(JSON.parse(localData));
-//     }
-//   }, []);
-
-//   const handleClickPrevPage = () => {
-//     if (pageNum > 1) setPageNum(pageNum - 1);
-//   };
-
-//   const handleClickNextPage = () => {
-//     if (topRatedMovies && pageNum < topRatedMovies?.total_pages) {
-//       setPageNum(pageNum + 1);
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (watchlist) {
-//       localStorage.setItem("watchlist", JSON.stringify(watchlist));
-//     }
-//   }, [watchlist]);
-
-//   const handleAddRemoveWatchlist = (id: string) => {
-//     // Make sure watchlist is not null and does not already include the movie.id
-//     if (!watchlist) {
-//       setWatchlist([id]);
-//     } else if (watchlist && !watchlist.includes(id)) {
-//       setWatchlist([...watchlist, id]);
-//     } else if (watchlist && watchlist.includes(id)) {
-//       // If item exists, lets remove it from the watchlist
-//       const newWatchlist = watchlist.filter((item) => item !== id);
-//       setWatchlist(newWatchlist);
-//     }
-//   };
-
-//   if (isPending) return <p>Page is loading data...</p>;
-//   if (error) return <p>An error occured: {error.message}</p>;
-
-//   return (
-//     <AuthContext.Provider
-//       value={{
-//         isLoggedIn,
-//         setIsLoggedIn,
-//         handleLogOut,
-//         idleTime,
-//         idleExpiresAt,
-//       }}
-//     >
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// }
