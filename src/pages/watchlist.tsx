c773cd012cf453ff35d1100a45026dca2d79f1fd
Mike S <mksoofian@gmail.com>
2025-02-17 20:46:55 -0500

    watchlist successfully fetches movies

--- src/pages/watchlist.tsx
+++ src/pages/watchlist.tsx
@@ -1,46 +1,51 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import { useWatchlistState } from "../providers/watchlistProvider";
-import { useQueries, useQuery } from "@tanstack/react-query";
-import { MovieApiResponse, WatchlistObj } from "../types/movie_types";
+// import { useQueries, useQuery } from "@tanstack/react-query";
+import { MovieApiResponse } from "../types/movie_types";
 
 function Watchlist() {
   const { watchlist } = useWatchlistState();
-  const [watchlistFromApi, setWatchlistFromApi] = useState(watchlist);
-  const [pageNum, setPageNum] = useState(1);
+  //   const [watchlistFromApi, setWatchlistFromApi] = useState([]);
+  //   const [pageNum, setPageNum] = useState(1);
 
-  const fetchWatchlistAPI = async (
-    title: string
-  ): Promise<MovieApiResponse> => {
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${
-          import.meta.env.VITE_TMDB_API_READ_ACCESS_TOKEN
-        }`,
-      },
-    };
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=${pageNum}`,
-      options
-    );
-    const data = await response.json();
-    return data;
-  };
-
-  const fetchMoviesFromWatchlist = useQueries({
-    queries: watchlist.map((movie) => {
-      return {
-        queryKey: ["movieKey", movie],
-        queryFn: fetchWatchlistAPI(movie.title),
-        // enabled: !!watchlist,
+  useEffect(() => {
+    const fetchWatchlistAPI = async (
+      title: string
+    ): Promise<MovieApiResponse> => {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${
+            import.meta.env.VITE_TMDB_API_READ_ACCESS_TOKEN
+          }`,
+        },
       };
-    }),
-    // queryKey: ["fetchMyWatchlist", pageNum],
-    // queryFn: fetchWatchlistAPI,
-  });
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=${1}`,
+        options
+      );
+      const data = await response.json();
+      return data;
+    };
 
-  console.log(watchlist);
+    watchlist.forEach(async (movie) => {
+      const movieResult = await fetchWatchlistAPI(movie.title);
+      console.log(movie, movieResult);
+    });
+  }, [watchlist]);
+
+  //   const fetchMoviesFromWatchlist = useQueries({
+  //     queries: watchlist.map((movie) => {
+  //       return {
+  //         queryKey: ["movieKey", movie],
+  //         queryFn: fetchWatchlistAPI(movie.title),
+  //         // enabled: !!watchlist,
+  //       };
+  //     }),
+  // queryKey: ["fetchMyWatchlist", pageNum],
+  // queryFn: fetchWatchlistAPI,
+  //   });
 
   //   if (isPending) return <p>Page is loading data...</p>;
   //   if (error) return <p>An error occured: {error.message}</p>;
