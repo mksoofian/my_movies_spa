6a90b2cf78612cb87d8bf7e78f36aa5849d22531
Mike S <mksoofian@gmail.com>
2025-02-28 15:56:58 -0500

    updated /search to a more basic starter from tanstackForm

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,51 +1,41 @@
-// import { useQuery } from "@tanstack/react-query";
-import { useState, useEffect } from "react";
+import { useForm } from "@tanstack/react-form";
 
 function Search() {
-  //   const [pageNum, setPageNum] = useState(1);
-  const [query, setQuery] = useState("");
-  const [debouncedQuery, setDebouncedQuery] = useState("");
+  const form = useForm({
+    defaultValues: {
+      movieQuery: "",
+    },
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      console.log(value);
+    },
+  });
 
-  useEffect(() => {
-    const debounceTimer = setTimeout(() => {
-      setDebouncedQuery(query);
-    }, 2000);
-    //cleanup
-    return () => clearTimeout(debounceTimer);
-  }, [query]);
-
-  console.log(debouncedQuery);
-  //   const fetchWatchlistAPI = async () => {
-  //     const options = {
-  //       method: "GET",
-  //       headers: {
-  //         accept: "application/json",
-  //         Authorization: `Bearer ${
-  //           import.meta.env.VITE_TMDB_API_READ_ACCESS_TOKEN
-  //         }`,
-  //       },
-  //     };
-  //     const response = await fetch(
-  //       `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=${pageNum}`,
-  //       options
-  //     );
-  //     const data = await response.json();
-  //     return data;
-  //   };
-  //   const {
-  //     isPending,
-  //     error,
-  //     data: searchedMovies,
-  //   } = useQuery<TopRatedMovies>({
-  //     queryKey: ["fetchMyWatchlist", pageNum],
-  //     queryFn: fetchWatchlistAPI,
-  //   });
   return (
-    <>
-      <h1>Search for Movies by Title</h1>
-      <input value={query} onChange={(e) => setQuery(e.target.value)} />
-    </>
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <div>
+          <form.Field
+            name="movieQuery"
+            children={(field) => (
+              <input
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+            )}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
-
 export default Search;
