3670aab4507535bd6dee300bc135683fc323fb26
Mike S <mksoofian@gmail.com>
2025-03-01 20:32:22 -0500

    replaced code with new MovieCard component

--- src/pages/watchlist.tsx
+++ src/pages/watchlist.tsx
@@ -2,11 +2,10 @@ import { useEffect, useState } from "react";
 import { useWatchlistState } from "../providers/watchlistProvider";
 // import { useQueries, useQuery } from "@tanstack/react-query";
 import { Movie, MovieApiResponse } from "../types/movie_types";
-import { dateFormatter } from "../utils/date-formatter";
-import { Check, Plus } from "lucide-react";
+import MovieCard from "../components/movie-card";
 
 function Watchlist() {
-  const { watchlist, setWatchlist, watchlistChecker } = useWatchlistState();
+  const { watchlist } = useWatchlistState();
   const [watchlistFromApi, setWatchlistFromApi] = useState<Movie[] | []>([]);
   const [isLoading, setIsLoading] = useState(true);
   //   const [pageNum, setPageNum] = useState(1);
@@ -49,18 +48,6 @@ function Watchlist() {
     setIsLoading(false);
   }, [watchlist]);
 
-  const handleAddRemoveWatchlist = (id: string, title: string) => {
-    // Make sure watchlist is not null and does not already include the movie.id
-    if (!watchlist) {
-      setWatchlist([{ id: id, title: title }]);
-    } else if (watchlist && !watchlistChecker(id)) {
-      setWatchlist([...watchlist, { id: id, title: title }]);
-    } else if (watchlist && watchlistChecker(id)) {
-      // If item exists, lets remove it from the watchlist
-      setWatchlist(watchlist.filter((item) => item.id !== id));
-    }
-  };
-
   //   const fetchMoviesFromWatchlist = useQueries({
   //     queries: watchlist.map((movie) => {
   //       return {
@@ -99,46 +86,7 @@ function Watchlist() {
       <h1>My Watchlist</h1>
       <div className="grid">
         {watchlistFromApi.map((movie) => {
-          return (
-            <div key={movie.id} className="grid-item">
-              <div className="card-image">
-                <div className="image-wrapper">
-                  <div className="voter-score">
-                    <p> {Math.floor(movie.vote_average * 10)}</p> <span>%</span>
-                  </div>
-
-                  <button
-                    className={
-                      "favorite-button" +
-                      (watchlistChecker(movie.id.toString())
-                        ? " favorite-button-checked"
-                        : " favorite-button-default")
-                    }
-                    onClick={() =>
-                      handleAddRemoveWatchlist(movie.id.toString(), movie.title)
-                    }
-                  >
-                    {watchlistChecker(movie.id.toString()) ? (
-                      <Check size={15} />
-                    ) : (
-                      <Plus size={15} />
-                    )}
-                  </button>
-
-                  <img
-                    className="poster"
-                    src={`https://media.themoviedb.org/t/p/w440_and_h660_face/${movie.poster_path}`}
-                  />
-                </div>
-              </div>
-              <div className="card-content">
-                <h4 className="movie-title">{movie.original_title}</h4>
-                <p className="movie-release-date">
-                  {dateFormatter(movie.release_date)}
-                </p>
-              </div>
-            </div>
-          );
+          return <MovieCard key={movie.id} movie={movie} />;
         })}
       </div>
     </>
