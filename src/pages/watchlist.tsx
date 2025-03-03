0bbe39b790d4c9ba32c23e1b3c3fdeb36d2e18ad
Mike S <mksoofian@gmail.com>
2025-03-03 08:44:41 -0500

    removed unused code

--- src/pages/watchlist.tsx
+++ src/pages/watchlist.tsx
@@ -6,47 +6,6 @@ import { errorCard } from "../utils/movie-card-sample-data";
 
 function Watchlist() {
   const { watchlist } = useWatchlistState();
-  //   const [watchlistFromApi, setWatchlistFromApi] = useState<Movie[] | []>([]);
-  //   const [isLoading, setIsLoading] = useState(true);
-  //   const [pageNum, setPageNum] = useState(1);
-
-  //   useEffect(() => {
-  //     const fetchWatchlistAPI = async (
-  //       title: string
-  //     ): Promise<MovieApiResponse> => {
-  //       const options = {
-  //         method: "GET",
-  //         headers: {
-  //           accept: "application/json",
-  //           Authorization: `Bearer ${
-  //             import.meta.env.VITE_TMDB_API_READ_ACCESS_TOKEN
-  //           }`,
-  //         },
-  //       };
-  //       const response = await fetch(
-  //         `https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=${1}`,
-  //         options
-  //       );
-  //       const data = await response.json();
-  //       return data;
-  //     };
-
-  //For each id in watchlist, fetch movies with that id
-  //If the result has a corresponding ID, add it to the watchlistFromApi
-  //     const tempWatchlist: Movie[] = [];
-  //     watchlist.forEach(async (movie) => {
-  //       const movieResult = await fetchWatchlistAPI(movie.title);
-  //       const movieMatchFound = movieResult.results.find(
-  //         (item) => item.id.toString() === movie.id
-  //       );
-  //       if (movieMatchFound) {
-  //         tempWatchlist.push(movieMatchFound);
-  //         // setWatchlistFromApi([...watchlistFromApi, movieMatchFound]);
-  //         setWatchlistFromApi(tempWatchlist);
-  //       }
-  //     });
-  //     setIsLoading(false);
-  //   }, [watchlist]);
 
   const fetchWatchlistAPI = async (title: string, id: string) => {
     const options = {
@@ -92,26 +51,6 @@ function Watchlist() {
   console.log(watchlistSearchResultsFromAPI);
   if (watchlistSearchResultsFromAPI.pending)
     return <p>Page is loading data...</p>;
-
-  //   if (
-  //     !watchlistSearchResultsFromAPI.pending &&
-  //     watchlistSearchResultsFromAPI.data.length > 0 &&
-  //     !watchlistSearchResultsFromAPI.data.some((movie) => movie !== undefined)
-  //   ) {
-  //     console.log(watchlistSearchResultsFromAPI.data);
-  //   }
-
-  // const handleAddRemoveWatchlist = (id: string, title: string) => {
-  //   // Make sure watchlist is not null and does not already include the movie.id
-  //   if (!watchlist) {
-  //     setWatchlist([{ id: id, title: title }]);
-  //   } else if (watchlist && !watchlistChecker(id)) {
-  //     setWatchlist([...watchlist, { id: id, title: title }]);
-  //   } else if (watchlist && watchlistChecker(id)) {
-  //     // If item exists, lets remove it from the watchlist
-  //     setWatchlist(watchlist.filter((item) => item.id !== id));
-  //   }
-  // };
 
   if (!watchlist) {
     return <h1>Loading...</h1>;
