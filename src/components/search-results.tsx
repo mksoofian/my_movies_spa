811bf223d90f56ab85def559da4ad96c15316e0e
Mike S <mksoofian@gmail.com>
2025-03-06 21:04:43 -0500

    created search-results component to move the cards and tansStack query into

---
+++ src/components/search-results.tsx
@@ -0,0 +1,83 @@
+// import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { MovieApiResponse } from "../types/movie_types";
+import MovieResultsGrid from "../components/movie-resuts-grid";
+
+function SearchResults({ query }: { query: string }) {
+  //   { pageNum }: { pageNum: number },
+  //   { setPageNum }: { setPageNum: (number: number) => number }
+  const [pageNum, setPageNum] = useState(1);
+
+  const fetchMovieAPI = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${
+          import.meta.env.VITE_TMDB_API_READ_ACCESS_TOKEN
+        }`,
+      },
+    };
+
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=${pageNum}`,
+      options
+    );
+
+    const data = await response.json();
+    console.log("api called");
+    return data;
+  };
+
+  const {
+    isPending,
+    isError,
+    error,
+    data: searchedMovies,
+  } = useQuery<MovieApiResponse>({
+    queryKey: ["fetchMovieAPI", query],
+    queryFn: fetchMovieAPI,
+  });
+
+  const handleClickPrevPage = () => {
+    if (pageNum > 1) setPageNum(pageNum - 1);
+  };
+
+  const handleClickNextPage = () => {
+    if (searchedMovies && pageNum < searchedMovies?.total_pages) {
+      setPageNum(pageNum + 1);
+    }
+  };
+
+  if (isPending) return <p>Loading data...</p>;
+  if (isError) {
+    return (
+      <>
+        <h1>Search for Movies by Title</h1>
+        <p>Error fetching movie data: {error.message}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <section>
+        <MovieResultsGrid results={searchedMovies.results} />
+      </section>
+      {searchedMovies.results.length > 0 ? (
+        <section>
+          <div className="pagination">
+            <button onClick={handleClickPrevPage}>prev</button>
+            <p>Page: {searchedMovies.page}</p>
+            <button onClick={handleClickNextPage}>next</button>
+          </div>
+        </section>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
+
+export default SearchResults;
