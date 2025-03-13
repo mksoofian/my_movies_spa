a5c9f1f2eb2f77decea297eb49a8b10cedf60249
Mike S <mksoofian@gmail.com>
2025-03-12 20:13:46 -0400

    created /src/mocks

---
+++ src/mocks/browser.js
@@ -0,0 +1,18 @@
+import { setupWorker } from "msw/browser";
+import { handlers } from "./handlers";
+
+export const worker = setupWorker(...handlers);
+
+// import { http, HttpResponse } from "msw";
+
+// export const handlers = [
+//   // Intercept "GET https://example.com/user" requests...
+//   http.get("https://api.themoviedb.org/3/search/movie", () => {
+//     // ...and respond to them using this JSON response.
+//     return HttpResponse.json({
+//       id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
+//       firstName: "John",
+//       lastName: "Maverick",
+//     });
+//   }),
+// ];
