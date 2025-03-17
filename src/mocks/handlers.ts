e5c8eba092031404714c71a58ba31fdd77534bae
Mike S <mksoofian@gmail.com>
2025-03-17 08:23:44 -0400

    successfully intercepted MSW with handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,8 @@ import { http, HttpResponse } from "msw";
 
 export const handlers = [
   // An example handler
-  http.get("https://api.themoviedb.org/3/search/", () => {
+  http.get("https://api.themoviedb.org/3/search/*", () => {
+    console.log("msw");
     return HttpResponse.json({ name: "John Maverick" });
   }),
 ];
