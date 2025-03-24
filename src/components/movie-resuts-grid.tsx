c0853b7e1ac8eb8f1867f8dacbb557e7d3d3097d
Mike S <mksoofian@gmail.com>
2025-03-24 19:51:40 -0400

    debugging watchlist length issues

--- src/components/movie-resuts-grid.tsx
+++ src/components/movie-resuts-grid.tsx
@@ -20,11 +20,17 @@ export default function MovieResultsGrid({
       setWatchlist([{ id: id, title: title }]);
     } else if (watchlist && !movieExists) {
       setWatchlist([...watchlist, { id: id, title: title }]);
+    } else if (watchlist.length === 1 && movieExists) {
+      // If ONLY ONE item exists, lets remove the watchlist from the watchlist
+      console.log("only one item in watchlist");
+      setWatchlist([]);
     } else if (watchlist && movieExists) {
       // If item exists, lets remove it from the watchlist
       setWatchlist(watchlist.filter((item) => item.id !== id));
+      console.log(watchlist.length);
     }
   };
+
   return (
     <div className="grid">
       {results.map((movie) => {
